550af4cc2388f0e7bf79395e1c407594afb8f696
kaiovasa1999-ux <veselin.d.valkanov@gmail.com>
2023-03-09 19:33:11 +0200

    automatic migration

---
+++ RentalProperties/DatabaseSeedomg/DatabaseSeeder.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RentalProperties.Data;
+using RentalProperties.Data.Models;
+
+namespace RentalProperties.DatabaseSeedomg
+{
+    public static class DatabaseSeeder
+    {
+        public static IApplicationBuilder Initialize(this IApplicationBuilder app)
+        {
+            using var scopedService = app.ApplicationServices.CreateScope();
+            var serviceProvider = scopedService.ServiceProvider;
+            var db = serviceProvider.GetRequiredService<ApplicationDbContext>();
+            db.Database.Migrate();
+            return app;
+        }
+    }
+}
--- RentalProperties/Program.cs
+++ RentalProperties/Program.cs
@@ -1,10 +1,10 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using RentalProperties.Data;
+using RentalProperties.DatabaseSeedomg;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
@@ -16,7 +16,6 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseMigrationsEndPoint();
@@ -24,9 +23,10 @@ if (app.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.Initialize();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
