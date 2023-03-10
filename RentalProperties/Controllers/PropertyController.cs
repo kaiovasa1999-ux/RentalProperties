3b53937b3c2f5bf9a9ba2c18b99d59d32bd53c5b
kaiovasa1999-ux <veselin.d.valkanov@gmail.com>
2023-03-10 19:06:16 +0200

    Created add form wich accept multiple files to be able to add

---
+++ RentalProperties/Controllers/PropertyController.cs
@@ -0,0 +1,74 @@
+﻿using Humanizer;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using RentalProperties.Data;
+using RentalProperties.Models.Property;
+
+namespace RentalProperties.Controllers
+{
+    public class PropertyController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly IWebHostEnvironment _webHost;
+
+        public PropertyController(ApplicationDbContext _db, IWebHostEnvironment webHost)
+        {
+            this._db = _db;
+            this._webHost = webHost;
+        }
+        public IActionResult Add()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddNewPropertyInputModel input)
+        {
+            var filePath = Path.GetTempFileName();
+            var files = input.Images;
+            foreach (var item in files)
+            {
+                if (!item.FileName.EndsWith(".jpg"))
+                {
+                    this.ModelState.AddModelError(item.FileName, "Invalid fail extension");
+                }
+            }
+            if (!ModelState.IsValid)
+            {
+                return this.View(input);
+            }
+            //var x = this._webHost.WebRootPath + "\\Images";
+            //var y = "C:\\Repos\\C#\\RentalProperties\\RentalProperties\\wwwroot\\Images";
+            using (FileStream fs = new FileStream(filePath, FileMode.Create))
+            {
+                foreach (var item in files)
+                {
+                    await item.CopyToAsync(fs);
+                }
+            };
+
+            return this.RedirectToAction("ThankYou");
+
+        }
+
+        public IActionResult ThankYou()
+        {
+            return this.View();
+        }
+
+        public IActionResult Edit(AddNewPropertyInputModel input)
+        {
+            if (!ModelState.IsValid)
+            {
+                return this.View(input);
+            }
+
+            return this.RedirectToAction("ThankYou");
+        }
+
+        public IActionResult Image()
+        {
+            return this.PhysicalFile(this._webHost.WebRootPath + @"/Images",".png");
+        }
+    }
+}
---
+++ RentalProperties/Models/Property/AddNewPropertyInputModel.cs
@@ -0,0 +1,35 @@
+﻿using RentalProperties.Data;
+using RentalProperties.Data.Models;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace RentalProperties.Models.Property
+{
+    using static DataConstants.PropertyConstants;
+
+    public class AddNewPropertyInputModel
+    {
+        [Required(AllowEmptyStrings =false, ErrorMessage ="There are only two options, for Rent or for Sale")]
+        [Display(Name = "Property Type")]
+        public PropertyType PropertyType { get; set; }
+
+        [Required]
+        public string Location { get; set; }
+
+        //[Url]
+        //[Required]
+        //[Display(Name = "Image")]
+        //public string ImageUrl { get; set; }
+
+        [Range(0, PropertyMaxPrice)]
+        [Required]
+        public double Price { get; set; }
+
+        [Required]
+        [DataType(DataType.MultilineText)]
+        [StringLength(DescriptionMaxLength,ErrorMessage ="The description is to long" )]
+        public string Description { get; set; }
+
+        public IEnumerable<IFormFile> Images { get; set; }
+    }
+}
